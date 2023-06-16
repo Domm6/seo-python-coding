5be13fe13c2a47070775bdf3ef5a7345b007fe2c
Dominic Parker <100439777+Domm6@users.noreply.github.com>
2023-06-16 09:27:44 -0700

    Create 2-inherits_from.py

---
+++ python-inheritance/2-inherits_from.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def inherits_from(obj, a_class):
+    """
+    Check if an object is an instance of a class that inherited (directly or indirectly) from the specified class.
+    """
+    obj_class = type(obj)
+    return issubclass(obj_class, a_class) and obj_class is not a_class

22d00d951451fb42338831e9f79e0f40a454f2bd
Dominic Parker <100439777+Domm6@users.noreply.github.com>
2023-06-18 12:28:32 -0700

    Update 5-base_geometry.py

--- python-inheritance/5-base_geometry.py
+++ python-inheritance/5-base_geometry.py
@@ -2,6 +2,8 @@
 """
 This class serves as a foundation for defining geometric shapes and operations.
 """
+
+
 class BaseGeometry:
     """
     A base class representing geometry.
@@ -18,9 +20,11 @@ class BaseGeometry:
     def integer_validator(self, name, value):
         """
         Validate the value as an integer.
-        
         """
         if not isinstance(value, int):
             raise TypeError(f"{name} must be an integer")
+        """
+        Validate the value as an integer.
+        """
         if value <= 0:
             raise ValueError(f"{name} must be greater than 0")

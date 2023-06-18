6b9beab590cb84d92d83c72302ac38c17341d2bd
Dominic Parker <100439777+Domm6@users.noreply.github.com>
2023-06-18 14:03:37 -0700

    Update 5-base_geometry.py

--- python-inheritance/5-base_geometry.py
+++ python-inheritance/5-base_geometry.py
@@ -1,27 +1,39 @@
 #!/usr/bin/python3
 """
-This class serves as a foundation for defining geometric shapes and operations.
-"""
+Module: base_geometry
 
+This module defines the BaseGeometry class, which serves as a foundation for defining geometric shapes and operations.
+"""
 
 class BaseGeometry:
     """
     A base class representing geometry.
+
+    This class serves as a foundation for defining geometric shapes and operations.
     """
+
     def area(self):
+        """
+        Calculate the area of the geometric shape.
+
+        Raises:
+            Exception: When called, as the area() method is not implemented in the base class.
+        """
         raise Exception("area() is not implemented")
-    """
-    Validate the value as an integer.
-        
-    """
+
     def integer_validator(self, name, value):
         """
         Validate the value as an integer.
+
+        Args:
+            name (str): The name of the value.
+            value: The value to be validated.
+
+        Raises:
+            TypeError: If the value is not an integer.
+            ValueError: If the value is less than or equal to 0.
         """
         if not isinstance(value, int):
             raise TypeError(f"{name} must be an integer")
-        """
-        Validate the value as an integer.
-        """
         if value <= 0:
             raise ValueError(f"{name} must be greater than 0")

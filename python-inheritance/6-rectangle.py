35b401513bab8c0a0410b63a2f3b2ca90bbc12ba
Dominic Parker <100439777+Domm6@users.noreply.github.com>
2023-06-18 14:09:15 -0700

    Create 6-rectangle.py

---
+++ python-inheritance/6-rectangle.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+This module defines the Rectangle class that represents a rectanlge.
+"""
+
+
+class Rectangle(BaseGeometry):
+    """
+    A class representing a rectangle.
+
+    This class inherits from the BaseGeometry class and adds functionality specific to rectangles.
+    """
+
+    def __init__(self, width, height):
+        """
+        Initialize a rectangle with given width and height.
+
+        Args:
+            width (int): The width of the rectangle.
+            height (int): The height of the rectangle.
+
+        Raises:
+            TypeError: If the width or height is not an integer.
+            ValueError: If the width or height is less than or equal to 0.
+        """
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height

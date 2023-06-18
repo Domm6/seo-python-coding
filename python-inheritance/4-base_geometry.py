29f188ae2005c1e0153e60890576952c19ca092b
Dominic Parker <100439777+Domm6@users.noreply.github.com>
2023-06-18 04:45:06 -0700

    Create 4-base_geometry.py

---
+++ python-inheritance/4-base_geometry.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+This class is for geometry
+"""
+
+
+class BaseGeometry:
+    """
+    A base class representing geometry.
+    """
+
+    def area(self):
+        """
+        Calculate the area of the geometric shape.
+
+        Raises:
+            Exception: When called, as the area() method is not implemented in the base class.
+        """
+        raise Exception("area() is not implemented")

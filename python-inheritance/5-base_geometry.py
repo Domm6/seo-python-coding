47d5abf4b03a218e7bb22cde8c43d5e85eb06d1d
Dominic Parker <100439777+Domm6@users.noreply.github.com>
2023-06-18 12:26:03 -0700

    Update 5-base_geometry.py

--- python-inheritance/5-base_geometry.py
+++ python-inheritance/5-base_geometry.py
@@ -7,14 +7,10 @@ This class serves as a foundation for defining geometric shapes and operations.
 class BaseGeometry:
     """
     A base class representing geometry.
-
     """
     def area(self):
         """
         Calculate the area of the geometric shape.
-
-        Raises:
-            Exception: When called, as the area() method is not implemented in the base class.
         """
         raise Exception("area() is not implemented")
     """

ce5a638610daf38fa52a095ae41edfbbdcd7e629
Dominic Parker <100439777+Domm6@users.noreply.github.com>
2023-06-21 16:42:50 -0700

    Update 5-base_geometry.py

--- python-inheritance/5-base_geometry.py
+++ python-inheritance/5-base_geometry.py
@@ -20,9 +20,7 @@ class BaseGeometry:
         raise Exception("area() is not implemented")
         
     def integer_validator(self, name, value):
-        """
-        Validating value
-        """
+        """Validating value"""
         if type(value) != int:
             raise TypeError(f"{name} must be an integer")
         if value <= 0:

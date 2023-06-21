ec32b3addeff3e86ece5fa7a4be6b952be28a456
Dominic Parker <100439777+Domm6@users.noreply.github.com>
2023-06-21 16:21:53 -0700

    Update 5-base_geometry.py

--- python-inheritance/5-base_geometry.py
+++ python-inheritance/5-base_geometry.py
@@ -19,14 +19,3 @@ class BaseGeometry:
         """
         raise Exception("area() is not implemented")
     def integer_validator(self, name, value):
-        """
-        Calculate the area of the geometric shape.
-
-        Raises:
-            Exception: When called, as the area() method\
-            is not implemented in the base class.
-        """
-        if not isinstance(value, int):
-            raise TypeError(f"{name} must be an integer")
-        if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")

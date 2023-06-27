775193f03945ef3695254404bce8ebda3f73f4e2
Dominic Parker <100439777+Domm6@users.noreply.github.com>
2023-06-26 17:21:12 -0700

    Update 6-rectangle.py

--- python-inheritance/6-rectangle.py
+++ python-inheritance/6-rectangle.py
@@ -2,19 +2,7 @@
 """
 Rectangle class that inherits from BaseGeometry
 """
-
-class BaseGeometry:
-    """ BaseGeometry class """
-    def area(self):
-        """ Raises an exception """
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """ Validates the value """
-        if type(value) != int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+import BaseGeometry
 
 class Rectangle(BaseGeometry):
     """ Rectangle class """
